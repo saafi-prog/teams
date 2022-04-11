4e5b8954a1bd9ee1185198e5016da13aa584db64
manal benallal <manelbenallal@hotmail.fr>
2022-04-11 16:39:35 +0200

    Removed accents

--- teams-db/data.dml.sql
+++ teams-db/data.dml.sql
@@ -12,10 +12,10 @@ INSERT INTO addresses (road, town_id) VALUES
 	('14 Rue de la Beaune', (SELECT t.id FROM towns t WHERE t.zip_code = '93100'));
 	
 INSERT INTO teams (name) VALUES
-	('Formateurs'), ('Pédagogique');
+	('Formateurs'), ('Pedagogique');
 	
 INSERT INTO members (first_name, last_name, home_address_id) VALUES
-	('Jérémy', 'BOJKO', (SELECT t.id FROM addresses t WHERE t.road = '14 Rue de la Beaune')),
+	('Jeremy', 'BOJKO', (SELECT t.id FROM addresses t WHERE t.road = '14 Rue de la Beaune')),
 	('Frank', 'MARSHALL', (SELECT t.id FROM addresses t WHERE t.road = '14 Rue de la Beaune')),
 	('Gloria', 'LUONG', (SELECT t.id FROM addresses t WHERE t.road = '14 Rue de la Beaune')),
 	('Gilles', 'DAUGUET', (SELECT t.id FROM addresses t WHERE t.road = '14 Rue de la Beaune'));
@@ -23,5 +23,5 @@ INSERT INTO members (first_name, last_name, home_address_id) VALUES
 INSERT INTO teams_members VALUES 
 	((SELECT t.id FROM teams t WHERE t.name = 'Formateurs'), (SELECT t.id FROM members t WHERE t.last_name = 'BOJKO')),
 	((SELECT t.id FROM teams t WHERE t.name = 'Formateurs'), (SELECT t.id FROM members t WHERE t.last_name = 'MARSHALL')),
-	((SELECT t.id FROM teams t WHERE t.name = 'Pédagogique'), (SELECT t.id FROM members t WHERE t.last_name = 'LUONG')),
-	((SELECT t.id FROM teams t WHERE t.name = 'Pédagogique'), (SELECT t.id FROM members t WHERE t.last_name = 'DAUGUET'));
+	((SELECT t.id FROM teams t WHERE t.name = 'Pedagogique'), (SELECT t.id FROM members t WHERE t.last_name = 'LUONG')),
+	((SELECT t.id FROM teams t WHERE t.name = 'Pedagogique'), (SELECT t.id FROM members t WHERE t.last_name = 'DAUGUET'));
